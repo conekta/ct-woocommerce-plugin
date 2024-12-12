683a8a7efe3adf1dde5faa8e068c9ad00e98bf50
José Nicolás Cedrón <nicolas.cedron@conekta.com>
2024-12-12 00:17:55 -0300

    AI-97: Update Readme (#46)
    * Upgrade version

--- conekta_plugin.php
+++ conekta_plugin.php
@@ -14,7 +14,7 @@ use Conekta\ApiException;
 
 class WC_Conekta_Plugin extends WC_Payment_Gateway
 {
-	public $version  = "5.0.7";
+	public $version  = "5.0.8";
 	public $name = "WooCommerce 2";
 	public $description = "Payment Gateway through Conekta.io for Woocommerce for both credit and debit cards as well as cash payments  and monthly installments for Mexican credit cards.";
 	public $plugin_name = "Conekta Payment Gateway for Woocommerce";
