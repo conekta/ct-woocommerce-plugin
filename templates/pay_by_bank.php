764df6c2b7f396e6754243e73bcd824bdf333952
Martín Agüera <maguera@conekta.com>
2025-10-07 16:54:49 -0300

    feat: [AI-457] - added new templates for the "Direct Payment" payment method

---
+++ templates/pay_by_bank.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Title   : Conekta Payment extension for WooCommerce
+ * Author  : Cristina Randall
+ * Url     : http://cristinarandall.com/
+ * License : http://cristinarandall.com/
+ */
+?>
+
+
+<span class='payment-errors required'></span>
+<?php echo esc_html($this->settings['description']); ?>
+<p style="margin-top: 10px;">
+    <small><?php echo esc_html(__('Serás redirigido a BBVA para autorizar el pago de forma segura.', 'woocommerce')); ?></small>
+</p>
